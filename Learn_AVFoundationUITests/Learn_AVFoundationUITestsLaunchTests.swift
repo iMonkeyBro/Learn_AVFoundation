22d022b139acb287b5d08ef35ca21b082b5d7b82
liuchaoqun <liuchaoqun.vendor@tetras.ai>
2022-03-02 15:01:52 +0800

    创建

---
+++ Learn_AVFoundationUITests/Learn_AVFoundationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Learn_AVFoundationUITestsLaunchTests.swift
+//  Learn_AVFoundationUITests
+//
+//  Created by 刘超群 on 2022/3/2.
+//
+
+import XCTest
+
+class Learn_AVFoundationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
