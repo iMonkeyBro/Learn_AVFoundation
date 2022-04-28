8e22fe24701693559023b7d62fddc78e9b4b13ab
liuchaoqun <51514170+iMonkeyBro@users.noreply.github.com>
2022-04-28 21:07:48 +0800

    player

---
+++ Learn_AVFoundation/Learn/CQPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  CQPlayer.swift
+//  Learn_AVFoundation
+//
+//  Created by 刘超群 on 2022/4/28.
+//
+
+import Foundation
+
+/**
+ 二次封装AVPlayer
+ */
+public final class CQPlayer {
+    
+}
+
+
+public protocol CQPlayerDelegate {
+    
+}
